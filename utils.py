9f947e520ca71a3030a1bc2e7f3f23881ffbd0c8
Claude FOTSO <fotso1claude@gmail.com>
2023-11-09 13:16:11 +0100

    Explications de la fonction add()

--- utils.py
+++ utils.py
@@ -16,6 +16,15 @@ class Trie:
         letters = list(word)
         node = self.content
 
+        """Pour chaque lettre du mot à la position n: 
+        La lettre fait partie des clés du dictionnaire à la prodondeur n ?
+            Non: 
+                On l'ajoute avec son booléen pour signaler s'il s'agit de la fin du mot ou pas.
+            Oui:
+                La lettre est la dernière du mot ?
+                 Oui: on signale que c'est la fin du mot.
+        On récupère le nœuds suivant de la trie
+        """
         for letter in letters:
             if letter not in node.keys():
                 node[letter] = {"is_word": word.endswith(letter)}
