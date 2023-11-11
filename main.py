69d3fca87ecb4a95a8b66c9d3d630635bc2e6205
Claude FOTSO <fotso1claude@gmail.com>
2023-11-11 11:29:09 +0100

    On affiche en console avec un parcours en profondeur, et il faut maintenant gérer les traits au moment de l'affichage

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ trie = Trie()
 for w in words:
     trie.add(w)
 
-print(trie.contains("créa"))
-print(trie.contains("création"))
+# print(trie.contains("créa"))
+# print(trie.contains("création"))
 
-# trie.affiche()
+trie.affiche()
--- utils.py
+++ utils.py
@@ -5,12 +5,17 @@ class Node:
     def __init__(self, value):
         self.value: str = value
         self.children: dict[str, Node] = {}
+        self.parent: (str, Node) = ()
         self.is_word: bool = False
+        self.visited: list = []
 
 
 @dataclass
-class Trie:
+class Trie(object):
     root: Node = Node("")
+    word = ""
+    i = 0
+    indent = 0
 
     def add(self, word: str):
         node = self.root
@@ -21,6 +26,7 @@ class Trie:
             else:
                 new_node = Node(letter)
                 node.children[letter] = new_node
+                new_node.parent = (node.value, node)
                 node = new_node
         node.is_word = True
 
@@ -33,3 +39,24 @@ class Trie:
             else:
                 return False
         return node.is_word
+
+    def affiche(self, node=root, prefix=""):
+        if node.is_word:
+            self.word = prefix
+            print(f"⏐")
+            self.word = ""
+
+        for child in node.children.values():
+            if self.i > 0:
+                print(f"⏐", end="")
+
+            print("   " * self.i + f"⏐⎯ {child.value} ", end="")
+            end = f"*[{prefix + child.value}]" if child.is_word else ""
+            print(end)
+
+            self.i += 1
+            self.affiche(child, prefix + child.value)
+
+            self.i -= 1
+
+
